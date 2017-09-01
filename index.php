713c57a52efd2d1c6d7bdc9dc7b99e7b8d1f6894
AirMaks <rm1010@yandex.ru>
2017-09-01 23:57:27 +0300

    999

--- index.php
+++ index.php
@@ -5,13 +5,31 @@ $error = false;
 
 
 
-           if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])
+$email = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $email = test_input($_POST["email"]);
+    }
+
+function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+
+
+
+if (isset($_REQUEST['name']) and isset($_REQUEST['phone'])
           and isset($_REQUEST['email']) and isset($_REQUEST['Position1'])
       and isset($_REQUEST['Position2']) and isset($_REQUEST['Position3'])
       and isset($_REQUEST['Position4']) and isset($_REQUEST['Position5'])
       and isset($_REQUEST['Position6']) and isset($_REQUEST['Position7'])
       and isset($_REQUEST['Position8']) and isset($_REQUEST['Position9'])
-     and isset($_REQUEST['Position10']) and isset($_REQUEST['Position11']))
+     and isset($_REQUEST['Position10']) and isset($_REQUEST['Position11'])
+           and isset($_REQUEST['age']) and isset($_REQUEST['country']) and isset($_REQUEST['gender']))
 
 
 {
@@ -29,6 +47,9 @@ $error = false;
      $Position9 = $_REQUEST['Position9'];
     $Position10 = $_REQUEST['Position10'];
     $Position11 = $_REQUEST['Position11'];
+    $age = $_REQUEST['age'];
+    $country = $_REQUEST['country'];
+    $gender = $_REQUEST['gender'];
 
 
 
@@ -38,15 +59,19 @@ $error = false;
          empty($Position4) || empty($Position5) ||
          empty($Position6) || empty($Position7) ||
          empty($Position8) || empty($Position9) ||
-        empty($Position10) || empty($Position11))
+        empty($Position10) || empty($Position11) ||
+    empty($age) || empty($country) || empty($gender))
 
     {
-        $error = 'Not all the strings filled?';
+      $error = 'Not all the strings filled?';
     }
 
     else
         {
         $row = 'Здравствуйте, ' . $name .
+            '. Ваш пол: ' . $gender .
+            '. Страна: ' . $country .
+            '. Ваш возраст: ' . $age .
             '. Ваш номер: ' . $phone .
             '. Ваш email: ' . $email .
             '. Ваш Вратарь: ' . $Position1 .
@@ -66,6 +91,7 @@ $error = false;
         $message = '<h3><strong>Thanks for the vote!</strong></h3>';
     }
 }
+
 
 
 
@@ -112,7 +138,14 @@ $error = false;
     <form class="form-horizontal" action="index.php" method="post">
 
 
-<select name="country" style="width: 250px">
+       <p>Your age:</p>
+           <input type="number" name="age" style="width: 50px" required><br>
+
+
+
+        <div class="form-horizontal" style="margin-top: 20px" >
+
+ <select name="country" style="width: 250px">
     <option value="AX">AALAND ISLANDS</option>
     <option value="AF">AFGHANISTAN</option>
     <option value="AL">ALBANIA</option>
@@ -339,184 +372,106 @@ $error = false;
     <option value="ZW">ZIMBABWE</option>
 
 </select><br>
-
+</div>
 
 
 <div class="custom-radio" style="margin-top: 20px">
 
         <label for="name" class="col-sm-2 control-label">SEX:</label><br>
-        <input type="radio" name="gender" value="mal"> Male<br />
-        <input type="radio" name="gender" value="fem"> Female<br />
+        <input type="radio" name="gender" value="Мужской" checked> Male<br />
+        <input type="radio" name="gender" value="Женский"> Female<br />
 
 </div>
 
 
 <div class="container" style="margin-top: 20px">
-        <div class="form-group">
+    <div style="width: 300px">
+
             <label for="name" class="col-sm-2 control-label">1.</label>
-            <div class="col-sm-4">
-                <input  type="text" style="font-family: Serif" name="Position1" class="form-control"
-                       placeholder="Goalkeeper" value="<?= $Position1 ?? '' ?>">
-            </div>
-        </div>
+            <input  type="text" style="font-family: Serif" name="Position1" class="form-control"
+                       placeholder="Goalkeeper" value="<?= $Position1 ?? '' ?>" required>
 
-
-
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">2.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position2"
-                       placeholder="Left defender" value="<?= $Position2 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position2"
+                       placeholder="Left defender" value="<?= $Position2 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">3.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position3"
-                       placeholder="Center back" value="<?= $Position3 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position3"
+                       placeholder="Center back" value="<?= $Position3 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">4.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position4"
-                       placeholder="Center back" value="<?= $Position4 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position4"
+                       placeholder="Center back" value="<?= $Position4 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">5.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position5"
-                       placeholder="Right defender" value="<?= $Position5 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position5"
+                       placeholder="Right defender" value="<?= $Position5 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">6.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position6"
-                       placeholder="Defensive midfielder" value="<?= $Position6 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position6"
+                       placeholder="Defensive midfielder" value="<?= $Position6 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">7.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position7"
-                       placeholder="Left midfielder" value="<?= $Position7 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position7"
+                       placeholder="Left midfielder" value="<?= $Position7 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">8.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position8"
-                       placeholder="Right midfielder" value="<?= $Position8 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position8"
+                       placeholder="Right midfielder" value="<?= $Position8 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">9.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position9"
-                       placeholder="Center midfielder" value="<?= $Position9 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position9"
+                       placeholder="Center midfielder" value="<?= $Position9 ?? '' ?>" required>
 
-        <div class="form-group">
             <label for="name" class="col-sm-2 control-label">10.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position10"
-                       placeholder="Forward" value="<?= $Position10 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position10"
+                       placeholder="Forward" value="<?= $Position10 ?? '' ?>" required>
 
-        <div class="form-group">
+
             <label for="name" class="col-sm-2 control-label">11.</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" style="font-family: Serif" name="Position11"
-                       placeholder="Forward" value="<?= $Position11 ?? '' ?>">
-            </div>
-        </div>
+            <input type="text" class="form-control" style="font-family: Serif" name="Position11"
+                       placeholder="Forward" value="<?= $Position11 ?? '' ?>" required>
+
+        <label for="name">Your Name</label>
+            <input type="text" class="form-control" style="font-family: Serif" name="name"
+               placeholder="Type your name" value="<?= $name ?? '' ?>" required>
+
+<div  class="phone_fieldWrapper">
+            <label for="name">Phone Number:</label>
+            <input name="phone" type="text" value="<?= $name ?? '' ?>" class="form-control" style="font-family: Serif" required
+                   placeholder="+0-000-000-00-00"/>
+</div>
+        <label for="name">EMAIL:</label>
+            <input type="email" class="form-control" style="font-family: Serif" name="email"
+               placeholder="example@mail.com" value="<?= $email ?? '' ?>" required>
 
 
-<div class="form-group">
-    <label for="name" class="col-sm-2 control-label">Your Name</label>
-    <div class="col-sm-4">
-        <input type="text" class="form-control" style="font-family: Serif" name="name"
-               placeholder="Type your name" value="<?= $name ?? '' ?>">
+
+
+
+<form action="index.php" method="post" required>
+    <div class="form-horizontal" style="margin-top: 20px">
+        <p>Hobby:</p>
+
+                    <input type="checkbox" value="sport">Sport</input><br>
+                    <input type="checkbox" value="photos">Photos</input><br>
+                    <input type="checkbox" value="coding">Coding</input><br>
+                    <input type="checkbox" value="snowboarding">Snowboarding</input>
     </div>
-</div>
+</form>
 
-<div class="form-group">
-    <label for="name" class="col-sm-2 control-label">Phone Number</label>
-    <div class="col-sm-4">
-        <input type="phone" class="form-control" style="font-family: Serif" name="phone"
-               placeholder="Type your phone number" value="<?= $phone ?? '' ?>">
-    </div>
-</div>
-
-<div class="form-group">
-    <label for="name" class="col-sm-2 control-label">EMAIL:</label>
-    <div class="col-sm-4">
-
-        <input type="email" class="form-control" style="font-family: Serif" name="email"
-               placeholder="Email" value="<?= $email ?? '' ?>">
-    </div>
-</div>
-
-
-
-
-        <div class="form-group">
-    <label for="name" class="col-sm-2 control-label">Your hobby:</label>
-    <p> <input type='checkbox' class='checkbox'
-               name="delete_1" value="123">
-        Sport</p>
-</div>
-
-<div class="form-group">
-    <p> <input type='checkbox' class='checkbox'
-               name="delete_2" value="456">
-        Music</p>
-</div>
-
-<div class="form-group">
-    <p> <input type='checkbox' class='checkbox'
-               name="delete_3" value="789">
-        Coding</p>
-</div>
-
-<div class="form-group">
-    <p> <input type='checkbox' class='checkbox'
-               name="delete_4" value="111">
-        Other</p>
-</div>
-
-
-
-<div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
+        <div class="col-sm-offset-2 col-sm-10" style="margin-top: 20px">
         <button type="submit" class="btn btn-primary">SEND TO FIFA</button>
-    </div>
-</div>
+        </div>
 
 </div>
 
 <p class="alert-danger col-md-4"><?= $error ?></p>
+
         </form>
 
+    <?php  endif; ?>
+    </div>
 
-
-
-
-
-
-<?php  endif; ?>
-</div>
-
-</body>
-</html>
+    </body>
+    </html>
